2940411aa8e98dd82347f3d0f7f9133d75e32fcd
Kirill <kiw197204@gmail.com>
2023-12-15 09:40:09 +0300

    feat: crypto done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  constructor(machine) {
+    if (typeof machine === 'boolean') {
+      this.machine = machine;
+    } else this.machine = true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(mes, key) {
+    if (typeof mes === 'undefined' || typeof key === 'undefined') throw Error('Incorrect arguments!');
+    mes = mes.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < mes.length; i++) {
+        let char = mes[i];
+        if (this.alpha.includes(char)) {
+          let charIndex = this.alpha.indexOf(char);
+          let keyCharIndex = this.alpha.indexOf(key[keyIndex % key.length]);
+
+          result += this.alpha[(charIndex + keyCharIndex) % this.alpha.length];
+          keyIndex++;
+        } else {
+          result += char;
+        }
+    }
+
+    if (!this.machine) result = result.split('').reverse().join('');
+    return result;
+  }
+  decrypt(mes, key) {
+    if (typeof mes === 'undefined' || typeof key === 'undefined') throw Error('Incorrect arguments!');
+    mes = mes.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < mes.length; i++) {
+        let char = mes[i];
+        if (this.alpha.includes(char)) {
+          let charIndex = this.alpha.indexOf(char);
+          let keyCharIndex = this.alpha.indexOf(key[keyIndex % key.length]);
+
+          result += this.alpha[(charIndex - keyCharIndex + this.alpha.length) % this.alpha.length];
+          keyIndex++;
+        } else {
+          result += char;
+        }
+    }
+
+    if (!this.machine) result = result.split('').reverse().join('');
+    return result;
   }
 }
 
